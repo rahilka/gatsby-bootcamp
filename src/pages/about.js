117d20e4549c067c2664f212f688575120fbaa1f
rahilka <rahilka.simonova@polarcape.com>
2019-12-07 10:54:48 +0100

    add additional pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About ME</h1>
+            <p>I am very enthusiastic about learning Gatsby.js and build few webpages</p>
+        </div>
+    )
+}
+
+export default AboutPage;

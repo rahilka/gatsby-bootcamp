68945954174683a7107f0778bb393d7b7485fbcf
rahilka <rahilka.simonova@polarcape.com>
2019-12-07 20:18:01 +0100

    add graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,10 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: 'Gatsby Bootcamp',
+    author: 'Rahilka Simonova'
+  },
   plugins: [
     'gatsby-plugin-sass'
   ]
